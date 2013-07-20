cdc16e0975e45ab068f3c2d17254bc1cd2a799db
Walfas <kirbehm@gmail.com>
2013-07-20 11:46:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Combined Tumblr Activity
+========================
+
+This userscript adds the notifications functionality of the Tumblr apps on 
+iOS and Android, where notifications include notes from all sideblogs.
+
---
+++ tumblr_combined_activity.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Combined Tumblr Activity
+// @namespace    http://ee.walfas.org
+// @version      0.1
+// @description  Pulls activity from all your sideblogs
+// @match        http://www.tumblr.com/*
+// @copyright    2013, Walfie
+// ==/UserScript==
+
+maxNotes = 50;
+
+if (/activity\/.*\?all/.test(window.location.href)) {
+    jQuery("#ui_activity_feed").addClass("ui_notes").empty();
+    jQuery(".section.first.divider").remove();
+    jQuery(".section.last").css("padding-top","10px");
+    users = [];
+
+    jQuery("#popover_blogs").find(".blog_title").each(function() {
+        users.push(jQuery(this).attr("href").replace("/blog/",""))
+    });
+
+    notes = [];
+
+    deferreds = users.map(function(user) {
+        return jQuery.ajax({
+            url: "http://www.tumblr.com/activity/" + user + "/day",
+        }).done(function (html) {
+            uiNotes = jQuery.makeArray(jQuery(html).find(".ui_note"));
+            notes = notes.concat(uiNotes);
+        });
+    });
+
+    jQuery.when.apply(null, deferreds).done(function() {
+        notes.sort(function(a,b) {
+            return getTimestamp(b) - getTimestamp(a);
+        });
+        notes = notes.slice(0,maxNotes);
+
+        // Check date boundaries
+        addDateSeparator(notes[0]);
+        for (var i=1; i<notes.length-1; i++) {
+            if (differentDays(notes[i],notes[i+1]))
+                addDateSeparator(notes[i+1]);
+        }
+
+        af = jQuery("#ui_activity_feed").addClass("ui_notes").empty().append(notes);
+    });
+
+    getTimestamp = function(obj) {
+        return jQuery(obj).attr('data-timestamp');
+    }
+
+    differentDays = function(note1, note2) {
+        var ts1 = getTimestamp(note1);
+        var ts2 = getTimestamp(note2);
+
+        var day1 = new Date(ts1 * 1000).setHours(0, 0, 0, 0);
+        var day2 = new Date(ts2 * 1000).setHours(0, 0, 0, 0);
+        return day1 !== day2;
+    }
+
+    addDateSeparator = function(elem) {
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        var timestamp = getTimestamp(elem);
+        var date = new Date(timestamp*1000);
+
+        var dayOfWeek = days[ date.getDay() ];
+        var day = date.getDate();
+        var month = months[ date.getMonth() ];
+        var dateString = dayOfWeek + ', ' + month + ' ' + day;
+
+        jQuery(elem).prepend(jQuery('<div/>', {'class': 'date_header clearfix'})
+            .append(jQuery('<div/>', {'class': 'part_full_date', 'text': dateString}))
+        );
+    }
+}
+else {
+    activity = jQuery(".activity");
+    activityLi = activity.parent().clone().appendTo("#dashboard_controls_open_blog");
+    activity.attr("href", activity.attr("href")+"?all").text("Notifications");
+}
